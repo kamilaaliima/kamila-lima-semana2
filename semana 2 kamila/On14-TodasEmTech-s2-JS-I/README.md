4b9e5cea0cb6f31972a083448b48634427041428
kamila lima <88142482+kamilaaliima@users.noreply.github.com>
2021-08-26 17:55:23 -0300

    Delete semana 2 kamila directory

--- semana 2 kamila/On14-TodasEmTech-s2-JS-I/README.md
+++
@@ -1,2 +0,0 @@
-# On14-TodasEmTech-s2-JS-I
-Turma Online 14 - Back-end | Javascript I
--- semana 2 kamila/semana 2/kamila.js
+++
@@ -1,9 +0,0 @@
-alert("SEJA BEM VINDO AO MEU SITE");
-
-alert(87 + 96);
-
-alert(5 * 8);
-
-let listaMercado = ["kiwi", "morango", "banana", "cafe", "pao"];
-alert(listaMercado) 
-
